eb94f5993056227ea88ffbb6490e2837df9a4172
JonathanDesarrollo2 <jjbsdesarrollo@gmail.com>
2026-09-08 18:32:07 -0400

    Respaldo antes de R8

--- services/notifications.ts
+++ services/notifications.ts
@@ -11,7 +11,6 @@
 
 
 
-/*
 import * as Notifications from 'expo-notifications';
 import * as Device from 'expo-device';
 import { Platform, Linking } from 'react-native';
@@ -71,13 +70,15 @@ export async function getFCMToken(): Promise<string | null> {
   return tokenResult.data;
 }
 
-  */
 // ========== BLOQUE TEMPORAL PARA EXPO GO ==========
 // Devuelve un token falso para que el resto de la app no falle.
 // Cuando compiles para producción, comenta este export y descomenta el bloque de arriba.
 
 
 
+/*
 export async function getFCMToken(): Promise<string | null> {
   return 'ExpoGoFakeToken';
 }
+  
+  */
